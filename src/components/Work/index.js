e3a65ca42d7792d2a765e8ddc7a719494feb2802
Noah Parker <noahtmparker@gmail.com>
2022-10-27 15:50:40 -0400

    fixed tailwind bug

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -9,9 +9,9 @@ import Note from '../../assets/images/note-taker.png';
 
 const Work = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-screen background text-gray-300">
       {/* Container */}
-      <div className="max-w-[1000px] bg-[#0a192f] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] bg-[#0a192f] mx-auto p-4 flex flex-col justify-center w-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-green-400">
             Work
